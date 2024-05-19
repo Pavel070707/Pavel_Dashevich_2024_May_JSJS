1802e20ed8f05820de211aed930322e850496ed3
Pavel070707 <8743060@gmail.com>
2024-05-19 12:58:47 +0200

    hw2

--- js/hw_1.js
+++ js/hw_1.js
@@ -1,1 +1,99 @@
 console.log('Hello World!');
+
+ // HW-2
+
+ // Task 1
+
+ let x = 20
+ let y = 58
+ let z = 42
+
+ let sum = x+y+z
+
+ console.log(sum)
+
+  // Task 2
+
+  let sec = 60
+  let min = 60 
+  let hours = 24
+  let day = 365
+
+  let myAge = 31
+
+  let result = myAge*day*hours*min*sec
+
+  console.log(result)  
+
+  // Task 3
+  let count = 42
+  let userName = '42'
+
+  console.log(count + "")
+  console.log(count.toString())
+  console.log(String(count))
+
+  console.log(+userName)
+  console.log(Number(userName))
+  console.log(parseFloat(userName))
+
+   // Task 4
+
+   let a = 1
+   let b = 2
+   let c = "белых медведей"
+
+   console.log(`${a}${b} ${c}`)
+
+      // Task 5
+
+      let q = 'доступ'
+      let w = 'доступ'
+      let e = 'доступ'
+      let r = 'доступ'
+      let t = 'доступ'
+
+      let lengthWords = q.length + w.length + e.length + r.length + t.length
+
+      console.log(lengthWords)
+
+            // Task 6
+
+            let o = 'доступ'
+            let u = 33
+            let i = true
+
+            console.log(`Variable: "${o}" have type:  ${typeof o}`)
+            console.log(`Variable: "${u}" have type:  ${typeof u}`)
+            console.log(`Variable: "${i}" have type:  ${typeof i}`)
+
+            // Task 7
+
+            // let myName = prompt("Введите ваше имя!")
+            // let age = prompt("Введите ваш возраст!")
+
+            // console.log(`Ваше имя: ${myName}. Ваш возраст:  ${age}`)
+
+            // Task 8
+
+            let s = 4
+            let d = 3
+            
+            s = s + d
+            d = s - d
+            s = s - d
+
+
+            console.log(d, s) 
+
+            // Task 9
+
+            let codeWord1 = "обернись";
+            let codeWord2 = "неужели";
+            let codeWord3 = "огурцы";
+            let codeWord4 = "липкие";
+            let codeWord5 = "?!";
+
+            let cipher = codeWord1[1]+codeWord2[1]+codeWord3[1]+codeWord4[1]+codeWord5[1]
+
+            console.log(cipher) 
